95141457bf19f8e077c857bd623deb57c00446fa
krinosx@gmail.com <krinosx@gmail.com>
2019-05-27 21:45:08 -0300

    Added documentation to Contoller Added some extra improvement suggestion to README Fixing some "coverage" warnings...

--- docs/README.md
+++ docs/README.md
@@ -27,11 +27,15 @@ OpenAPI standards. With this approach we can make sure that all api users will k
 Api methods.
 With Swagger it is also possible to automate the API client generation with some tools.
 
+### Implement HATEOAS navigation
+With some extra time we can apply the Hypermedia As The Engine Of Application State (HATEOAS) pattern ir order to make
+the API self explanatory and allow clients to easy navigate our API.
+
 ### Use a better and already tested comparision API
 There is some comparision APIs wide used and well tested that may be a improvement to current implementation. I think
 that for tasks such text comparision we may leverage already existent solutions instead of creating our own.
 
-### Check if comparision data may be transient
+### Check if comparision data can be transient
 If comparision data may be ephemeral, if it does not need to be stored forever, we should check if the API call can
 be simplified.
 It will be good to send both data (left and right) within a single request and get the comparision result as a response.
@@ -43,8 +47,18 @@ the API calls. If a lot of users are doing parallel calls it can cause a mess wi
 create another endpoint to generate an ID before the user start sending data or use the `stateless` approach from
 previous suggestion, but saving only the comparision result and providing an ID to be used for later consultation.
 
+### Security aspects
+If this application is to be exposed on Internet we must take some security measures. A common approach is to have an API Gateway
+exposed to the internet, in a DMZ network, that route the http requests to our application in a secure network. All the
+authentication and attack prevention can be implemented by the API Gateway, so we can keep our app code simple.
+If the application is intended to stay on intranet perimeter we can use a simple JWT token to keep track of our local
+users and create some audit or access control process if needed.
 
+### Add Logging and monitoring mechanism
+I did not expend time creating a log mechanism. I did not implement any log at all. If this application was intended to
+go to a production environment I will put some effort on logging to diagnose possible problems. I will also put some
+health monitoring in order to know how the application is performing and help the decide if its time to scale up or down.
 
-
-
-
+### Tune Spring Boot configuration
+I let most of SpringBoot autoconfiguration features enabled. For a production deployment I would do a double check and
+disable any unused features in order to make the application secure and lean.
--- src/main/java/com/giulianobortolassi/waes/diff/DiffController.java
+++ src/main/java/com/giulianobortolassi/waes/diff/DiffController.java
@@ -3,7 +3,6 @@ package com.giulianobortolassi.waes.diff;
 
 import com.giulianobortolassi.waes.comparator.Base64Comparator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,24 +17,23 @@ public class DiffController {
     @Autowired
     private DiffRequestRepository repository;
 
-
     /**
-     *
-     *  Base64 must be considered as text/plain data
+     * Set the right side data and persist the DiffRequest.
+     * Base64 must be considered as text/plain data
      *
      * - Configured the base64data to be 'optional' so I can deal with the error independent of spring handlers.
      * I prefer to use it in this application to keep things simple. In applications/api with lots of methods
      * it will be best to configure custom Exceptions and Handlers to do the job.
      *
-     *
-     * @param id
-     * @param base64data
+     * @param id the DiffRequest ID. If no request exist with the given ID, a new one is created.
+     * @param base64data some text plain data.
+     * @return a HTTP 200 if everything went right. An error if something strange happens.
      */
     @RequestMapping(method = RequestMethod.PUT, value = "/left")
-    public ResponseEntity leftSide( @PathVariable("id") String id, @RequestBody(required = false) String base64data ){
+    public  ResponseEntity<DiffResponse> leftSide( @PathVariable("id") String id, @RequestBody(required = false) String base64data ){
 
         if( id == null || base64data == null || base64data.isEmpty() ){
-            return new ResponseEntity(new DiffResponse("ERROR", "id and body data are mandatory"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new DiffResponse("ERROR", "id and body data are mandatory"), HttpStatus.BAD_REQUEST);
         }
 
         DiffRequest request = repository.getRequest(id);
@@ -53,17 +51,24 @@ public class DiffController {
             // TODO: Log it to alert some monitoring tool
             e.printStackTrace();
             // It should never happen. We check and set the ID.
-            return new ResponseEntity(new DiffResponse("ERROR", "Internal server error."), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new DiffResponse("ERROR", "Internal server error."), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * Set the right side data and persist the DiffRequest.
+     *
+     * @param id the DiffRequest ID. If no request exist with the given ID, a new one is created.
+     * @param base64data some text plain data.
+     * @return a HTTP 200 if everything went right. An error if something strange happens.
+     */
     @RequestMapping(method = RequestMethod.PUT, value = "/right")
     public ResponseEntity<DiffResponse> rightSide( @PathVariable("id") String id, @RequestBody(required = false) String base64data ){
 
         if( id == null || base64data == null || base64data.isEmpty() ){
-            return new ResponseEntity(new DiffResponse("ERROR", "id and body data are mandatory"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(new DiffResponse("ERROR", "id and body data are mandatory"), HttpStatus.BAD_REQUEST);
         }
 
         DiffRequest request = repository.getRequest(id);
@@ -80,20 +85,26 @@ public class DiffController {
             // TODO: Log it to alert some monitoring tool
             e.printStackTrace();
             // It should never happen. We check and set the ID.
-            return new ResponseEntity(new DiffResponse("ERROR", "Internal server error."), HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new DiffResponse("ERROR", "Internal server error."), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
         return ResponseEntity.ok().build();
     }
 
-
+    /**
+     * Get the DiffRequest from database and execute the comparision. If the comparision was already executed and no data
+     * was changed since the last execution, so the 'cached' result is returned.
+     *
+     * @param id the DiffRequest id.
+     * @return The result of comparision or an error message.
+     */
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<DiffResponse> compare(@PathVariable("id") String id) {
 
 
         DiffRequest request = repository.getRequest(id);
         if( request == null ) {
-            return new ResponseEntity(
+            return new ResponseEntity<>(
                     new DiffResponse("ERROR","No request found with given ID"),
                     HttpStatus.NOT_FOUND);
         }
@@ -105,10 +116,11 @@ public class DiffController {
 
             result = request.getResult(comparator);
 
-            /**
+            /*
              * I think there must be a better way to do that check.
-             * It can be handled with some versioning.
-             * I will put that in the improvement list.
+             * It can be handled with some versioning mechanism.
+             *
+             * I will consider that in the improvement list.
              */
             if (!wasEvaluated) {
                 repository.saveRequest(request);
@@ -117,14 +129,14 @@ public class DiffController {
             return new ResponseEntity<>( result, HttpStatus.OK );
 
         } catch (Exception e) {
-
             // Deal with presentation logic.
             if( e instanceof IllegalStateException ) {
-                return new ResponseEntity(
+                return new ResponseEntity<>(
                         new DiffResponse("ERROR","Request is not ready to be processed."),HttpStatus.BAD_REQUEST);
 
             } else {
-                return new ResponseEntity(
+                /* Its wise to implement some Logging mechanism to track the unexpected errors. */
+                return new ResponseEntity<>(
                         new DiffResponse("ERROR","Unexpected error."),HttpStatus.INTERNAL_SERVER_ERROR);
 
             }
